d39a56e6a8e6cb112e52ce509e016b54f6accc7a
wofenglelc <momoliuchuan@foxmail.com>
2025-06-07 23:42:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bookmark/', // ⚠️ 要根据你的仓库名填写
+  base: '/', // ⚠️ 要根据你的仓库名填写
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
